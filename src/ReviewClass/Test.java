94f3fe248d3098d7b27e6a96634280cadbb1890e
AMartinez213 <AllanLA2000@gmail.com>
2022-10-06 22:21:40 -0400

    Test

---
+++ src/ReviewClass/Test.java
@@ -0,0 +1,15 @@
+package ReviewClass;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        String name="God";
+        String gname="I love you ";
+
+        System.out.println(gname+name);
+
+
+    }
+
+}
